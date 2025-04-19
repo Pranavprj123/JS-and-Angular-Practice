f57673c575ee06c04e5e462f895f689f54c0fcee
Pranavprj <jadhavpranav64@gmail.com>
2025-04-19 14:24:51 +0530

    services and ngstyles

--- AngularPV/firstapp/src/app/attributedirectives/attributedirectives.component.ts
+++ AngularPV/firstapp/src/app/attributedirectives/attributedirectives.component.ts
@@ -8,10 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './attributedirectives.component.css'
 })
 export class AttributedirectivesComponent {
+
+  darktheme:boolean=true;
+  isHighlighted:boolean=false;
   classes:any= 
   {
     bg:'bg-primary',
     tc:'text-white',
     m:'m-3',
   }
+  toggle()
+  {
+    this.darktheme=!this.darktheme;
+    }
+
+  boxStyles():any
+  {
+    return{'background-color':this.isHighlighted?'red':'white',
+      'color':'green'
+    }
+  }
+
+  changeHighlight()
+  {
+    this.isHighlighted=!this.isHighlighted;
+  }
 }
+
+
